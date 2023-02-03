1fb84597d0f861a9e735889ef15d50e816aa1fd3
dab <dab@dab.com>
2023-02-03 20:59:17 +0300

    first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Catalog from "./components/pages/Catalog";
 
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 
 
