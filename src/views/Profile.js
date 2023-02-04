1a200d029660dc5a01ad0a366238ee7441c8c769
dab <dab@dab.com>
2023-02-04 08:54:34 +0300

    2

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -3,12 +3,13 @@ import {useTelegram} from "../hooks/useTelegram";
 
 const Profile = (props) => {
     const {user} = useTelegram();
-    console.log(user.username)
     return (
         <div className={'profile'}>
             <img src={user?.photo} className="profile__image" alt={user?.name}/>
             <div className="profile__name">
                 {user?.name}
+                wqeqwe
+                {JSON.stringify(user)}
             </div>
         </div>
     );
