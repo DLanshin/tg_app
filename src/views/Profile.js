1d1f2685e151dd907b0b3bc09c7988c8208a662f
dab <dab@dab.com>
2023-02-04 09:16:59 +0300

    2

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -7,8 +7,13 @@ const Profile = (props) => {
     return (
         <div className={'profile'}>
             <img src={user?.photo_url ? user.photo_url : avatarImg} className="profile__image" alt={user?.name}/>
-            <div className="profile__name">
-                {user?.first_name} {user?.last_name} qwdsa as sa
+            <div className="profile__content">
+                <div className="profile__title">
+                    {user?.first_name} {user?.last_name} qwdsa as sa
+                </div>
+                <div className="profile__subtitle">
+                    qwdsa as sa
+                </div>
             </div>
         </div>
     );
