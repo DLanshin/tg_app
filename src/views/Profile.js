e5bed4991e54e2c4abc360fedf6cc28aa6bbef8f
dab <dab@dab.com>
2023-02-04 08:59:59 +0300

    2

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import {useTelegram} from "../hooks/useTelegram";
+import avatarImg from "../assets/images/avatar.png"
 
 const Profile = (props) => {
     const {user} = useTelegram();
     return (
         <div className={'profile'}>
-            <img src={user?.photo} className="profile__image" alt={user?.name}/>
+            <img src={user?.photo_url ? user.photo_url : avatarImg} className="profile__image" alt={user?.name}/>
             <div className="profile__name">
-                {user?.name}
-                wqeqwe
-                {JSON.stringify(user)}
+                {user?.first_name} {user?.last_name}
             </div>
         </div>
     );
