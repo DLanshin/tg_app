af6e2a2271ead34df6e232f9f7de1ce41fda619f
dab <dab@dab.com>
2023-02-04 10:14:46 +0300

    2

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -2,9 +2,15 @@ import React from 'react';
 import {useTelegram} from "../hooks/useTelegram";
 import UserInfo from "../components/Profile/UserInfo/UserInfo";
 import List from "../components/List/List";
+import catalogIcon from "../assets/images/icons/coffee.svg";
+import basketIcon from "../assets/images/icons/basket.svg";
+import orderIcon from "../assets/images/icons/order.svg";
+import closeIcon from "../assets/images/icons/close.svg";
+import { ReactSVG } from 'react-svg'
+
 
 const Profile = (props) => {
-    const {user} = useTelegram();
+    const {user, onClose} = useTelegram();
     const userInfoList = [
         {
             name:"ID",
@@ -23,6 +29,42 @@ const Profile = (props) => {
         <div className={'profile'}>
             <UserInfo/>
             <List list={userInfoList}/>
+            <div className="grid-list">
+                <div className="grid-list__item">
+                    <div className="grid-list__icon">
+                        <ReactSVG src={catalogIcon} />
+                    </div>
+                    <div className="grid-list__text">
+                        Каталог
+                    </div>
+                </div>
+                <div className="grid-list__item">
+                    <div className="grid-list__icon">
+                        <span className="grid-list__hint">0</span>
+                        <ReactSVG src={basketIcon}/>
+                    </div>
+                    <div className="grid-list__text">
+                        Корзина
+                    </div>
+                </div>
+                <div className="grid-list__item">
+                    <div className="grid-list__icon">
+                        <span className="grid-list__hint">0</span>
+                        <ReactSVG src={orderIcon}/>
+                    </div>
+                    <div className="grid-list__text">
+                        Мои заказы
+                    </div>
+                </div>
+                <div className="grid-list__item" onClick={onClose}>
+                    <div className="grid-list__icon">
+                        <ReactSVG src={closeIcon}/>
+                    </div>
+                    <div className="grid-list__text">
+                        Закрыть
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
