8e72d015c1c2d116dcf958fb95e704da9c82f1ec
dab <dab@dab.com>
2023-02-22 15:58:26 +0300

    2

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import productImage from "../assets/images/product.jpg";
+import {cartReducer} from "./reducers/cart/cart-reducer";
+import {cartTotalReducer} from "./reducers/cart/cart-total-reducer";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {categoriesReducer} from "./reducers/catalog/categories-reducer";
+import {productsReducer} from "./reducers/catalog/products-reducer";
+import thunk from "redux-thunk";
+import {userReducer} from "./reducers/user/user-reducer";
+
+let rootReducer = combineReducers({
+    cart: cartReducer,
+    cartTotal: cartTotalReducer,
+    categoriesList: categoriesReducer,
+    products: productsReducer,
+    user_info: userReducer
+
+})
+export let store = createStore(rootReducer, applyMiddleware(thunk));
+
+window.state = store.getState();
