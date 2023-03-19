b0b85f17aa036081e37dacaa7f1581387eac26b8
dab <dab@dab.com>
2023-03-19 21:20:39 +0300

    2

--- src/http/index.js
+++ src/http/index.js
@@ -5,12 +5,16 @@ export const setBotID = bot_id =>{
     localStorage.setItem("bot_id", bot_id);
 }
 
+axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 const $api = axios.create({
     // withCredentials: true,
-    baseURL:`${process.env.REACT_APP_API_URL}`
+    baseURL:`${process.env.REACT_APP_API_URL}`,
 });
 const authInterceptor = config => {
-    config.headers.authorization = `Bearer ${localStorage.getItem('accessToken')}`
+    config.headers.authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+    config.headers.post['Content-Type'] ='application/json;charset=utf-8';
+    config.headers.post['Access-Control-Allow-Origin'] = '*';
     return config;
 }
 
