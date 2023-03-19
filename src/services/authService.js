3c8cd15c6bc2cd9c84f8ed4169facb225539711e
dab <dab@dab.com>
2023-03-19 21:36:56 +0300

    2

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,7 +6,8 @@ import axios from "axios";
 export const login = (bot_id, user_id) => {
     return async function (dispatch){
         localStorage.setItem("bot_id", bot_id);
-        const {data} =  await axios.get(`${process.env.REACT_APP_API_URL}${bot_id}/auth/login?user_id=${user_id}`,{ withCredentials: false })
+        debugger
+        const {data} =  await axios.get(`${process.env.REACT_APP_API_URL}${bot_id}/auth/login?user_id=${user_id}`,{ withCredentials: true })
         localStorage.setItem("accessToken", data.data.token);
         localStorage.setItem("refreshToken", data.data.refresh_token);
         dispatch(authUserAction(data.data));
