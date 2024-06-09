12147fcfedb22c8705f8e31336ab2fd7ec1b3e90
dab <rse@rep.earth>
2024-06-09 11:49:43 +0300

    fixes

--- src/components/Cart/MiniCart.jsx
+++ src/components/Cart/MiniCart.jsx
@@ -1,32 +1,30 @@
-import React from 'react';
+import React from 'react'
+import { CART_ROUTE } from '../../utils/consts'
+import {NavLink} from "react-router-dom";
 
-const MiniCart = ({elements}) => {
-    if(!elements || !elements?.length){
-        return (<></>)
-    }
-    console.log(elements);
-    return (
-        <div className={'mini-cart'}>
-            <div className="mini-cart__title">
-                В корзине
-            </div>
-            <div className="mini-cart__list">
-            {
-                elements.map(item => (
-                    <div key={item.id} className={'mini-cart__item'}>
-                        <div className={"mini-cart__item-info"}>
-                            <div className={"mini-cart__item-title"}>{item.title}</div>
-                            <div className={"mini-cart__item-hint"}>{item.sku_title}</div>
-                        </div>
-                        <div className={"mini-cart__item-count"}>
-                            {item.count} шт.
-                        </div>
-                    </div>
-                ))
-            }
-            </div>
-        </div>
-    );
-};
+const MiniCart = ({ elements }) => {
+	if (!elements || !elements?.length) {
+		return <></>
+	}
+	console.log(elements)
+	return (
+		<div className={'mini-cart'}>
+			<div className='mini-cart__title'>В корзине</div>
+			<div className='mini-cart__list'>
+				{elements.map(item => (
+					<NavLink to={CART_ROUTE}>
+						<div key={item.id} className={'mini-cart__item'}>
+							<div className={'mini-cart__item-info'}>
+								<div className={'mini-cart__item-title'}>{item.title}</div>
+								<div className={'mini-cart__item-hint'}>{item.sku_title}</div>
+							</div>
+							<div className={'mini-cart__item-count'}>{item.count} шт.</div>
+						</div>
+					</NavLink>
+				))}
+			</div>
+		</div>
+	)
+}
 
-export default MiniCart;
+export default MiniCart
